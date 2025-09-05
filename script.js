115f5cf34a826010c92a6c584bae3ba36bfed616
RaviVerma251 <raviverma90264@gmail.com>
2025-09-05 23:43:09 +0530

    Fix: Corrected the entire CSS file

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 // Initialize AOS (Animate on Scroll)
 AOS.init({
-  duration: 1000, // Animation will last for 1 second
-  once: true,     // Animation will only happen once per element
-  offset: 120,    // Animation starts 120px before the element is in view
+    duration: 1000, // Animation will last for 1 second
+    once: true,     // Animation will only happen once per element
+    offset: 120,    // Animation starts 120px before the element is in view
 });
